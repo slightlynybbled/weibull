2fefcf0dc7bbb6b470e775bb4b87f75db8ad8cd0
Jason R. Jones <slightlynybbled@gmail.com>
2019-11-04 06:53:06 -0500

    Adding depreciation warning.

--- weibull/__init__.py
+++ weibull/__init__.py
@@ -1,5 +1,10 @@
+import warnings
+
 from weibull.version import __version__
 from weibull.weibull import Analysis, Design, Weibayes, ParameterError
 
 __all__ = ['__version__',
            'Analysis', 'Design', 'Weibayes', 'ParameterError']
+
+warnings.warn('The "weibull" module is being superceded by the "reliability" module and will no longer be maintained. '
+              'Please refactor your code for this alternative model and thank you for using weibull!')
